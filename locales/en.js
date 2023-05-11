660cdee44ae22948d62be05f055767f0510be17f
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2023-05-11 14:57:00 +0500

    mobile

--- locales/en.js
+++ locales/en.js
@@ -3,6 +3,7 @@ const en = {
     "intro desc": "We help you find the perfect resort that suits your needs and budget. We offer a wide range of services to make your ski holiday as comfortable and enjoyable as possible.",
     "resorts": "Resorts",
     "articles": "Articles",
+    "saved": "Saved",
     "order a website": "Order a website",
     "search": "Search by name",
     "sochi": "RESORTS OF SOCHI",
